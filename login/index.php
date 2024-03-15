b7248419efeb518add079b21afaa202b1dc30373
MouhammadRaad <mohamadraad515@gmail.com>
2024-03-15 19:41:30 +0200

    To do list

--- login/index.php
+++ login/index.php
@@ -20,8 +20,10 @@
                 <i class="fa-solid fa-lock fa-lg"></i>
                 <input type="password" id="pass" name="pass" placeholder="Create Password" required></br></br>
                 <input type="submit" id="btn" value="SignUp" name = "submit"/>
+                <p>Already have an account? <a href="login.php">Login here</a>.</p>
             </form>
       </div>
+      
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
 </html>
